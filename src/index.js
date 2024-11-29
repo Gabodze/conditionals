fb86e2429a79ccb7704443aeb219a2f0d06c3f4b
Gabodze <mgabodze96@gmail.com>
2024-11-29 11:13:28 +0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function App() {
+  const x = 5;
+
+  if (x > 0) {
+    return (
+      <div
+        style={{
+          backgroundColor: 'green',
+          margin: '20px 20px',
+        }}
+      ></div>
+    );
+  } else {
+    return (
+      <div
+        style={{
+          backgroundColor: 'red',
+          margin: '20px 20px',
+        }}
+      ></div>
+    );
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
